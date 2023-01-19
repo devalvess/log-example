fb7471e063bc1283a7804c422336ca8b9e297861
Edilson Santos <edilsonsantosexsp@gmail.com>
2023-01-18 23:14:25 -0300

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
     public function sub($num1, $num2)
     {
         $sub = $num1 - $num2;
-        logger()->debug('Sub Feita ', compact('num1', 'num2', 'sub'));
+        logger()->debug('Sub Feita', compact('num1', 'num2', 'sub'));
 
         return $sub;
     }
